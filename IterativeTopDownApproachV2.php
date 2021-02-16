c9ec30886d91636f9128f1549b4cdb6136118c7a
Liutauras Kavaliauskas <22213927+liutauraskavaliauskas@users.noreply.github.com>
2021-02-16 12:30:32 +0200

    Add iterative top down approach v2

---
+++ IterativeTopDownApproachV2.php
@@ -0,0 +1,26 @@
+<?php
+
+//Let's get rid of the need to use all of that space and instead use the minimum amount of space required.
+//We can achieve O(1)O(1) space complexity by only storing the value
+//of the two previous numbers and updating them as we iterate to N.
+
+class IterativeTopDownApproach
+{
+    public function fibonacciNumber(int $number): int
+    {
+        $sum = 0;
+        $firstSummand = 0;
+        $secondSummand = 1;
+
+        $i = 0;
+
+        while ($i < $number) {
+            $sum = $firstSummand + $secondSummand;
+
+            $firstSummand = $secondSummand;
+            $secondSummand = $sum;
+        }
+
+        return $sum;
+    }
+}
